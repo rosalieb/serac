c85a8fdbf9338eb2efb7954b431749ff1e0d9d0e
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2019-07-30 17:11:08 -0400

    edit init message

--- R/init.R
+++ R/init.R
@@ -1,12 +1,7 @@
 .onAttach = function(libname, pkgname){
   packageStartupMessage("")
-  packageStartupMessage("Hi there, welcome to serac, a package for ShortlivEd RAdionuclide")
-  packageStartupMessage("Chronology of recent sediment cores.")
+  packageStartupMessage("Hi there, welcome to serac, a package for ShortlivEd RAdionuclide Chronology of recent sediment cores.")
   packageStartupMessage("")
-  packageStartupMessage("If you need a little help to get started, type in help_serac()")
-  packageStartupMessage("or vignette('serac') [if you selected build_vignettes = TRUE]")
-  packageStartupMessage("in the console.")
-  packageStartupMessage("If you're using serac for the first time, you may want to use ")
-  packageStartupMessage("the function user_infos() to enter metadata.")
-  packageStartupMessage("")
+  packageStartupMessage("If you need a little help to get started, type in help_serac() or vignette('serac')")
+  packageStartupMessage("If you're using serac for the first time, you may want to use the function user_infos() to enter metadata.")
 }
