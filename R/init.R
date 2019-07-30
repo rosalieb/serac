547db98ee6edd7fa379625cc1d1b540a737cb7fd
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2019-07-30 16:58:04 -0400

    add initialization function

---
+++ R/init.R
@@ -0,0 +1,11 @@
+.onAttach = function(libname, pkgname){
+  packageStartupMessage("Hi there, welcome to serac, a package for ShortlivEd RAdionuclide")
+  packageStartupMessage("Chronology of recent sediment cores.")
+  packageStartupMessage("")
+  packageStartupMessage("If you need a little help to get started, type in help_serac()")
+  packageStartupMessage("or vignette('serac') [if you selected build_vignettes = TRUE]")
+  packageStartupMessage("in the console.")
+  packageStartupMessage("If you're using serac for the first time, you may want to use ")
+  packageStartupMessage("the function user_infos() to enter metadata.")
+  packageStartupMessage("")
+}
