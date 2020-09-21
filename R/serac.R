c21781e3b18b9e7bf0872f982ece87fe66d28e75
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2020-09-21 10:42:46 -0400

    probleme de decalage quand on utilise mass_depth, les lignes du tableau de sortie sont décalés

--- R/serac.R
+++ R/serac.R
@@ -1539,8 +1539,8 @@ serac <- function(name = "", model = c("CFCS"), Cher = NA, NWT = NA, Hemisphere 
     } else {
       colnames(output_agemodel_CIC_inter) <- c("depth_avg_mm", "mass_depth_g.cm.2","BestAD_CIC", "MinAD_CIC", "MaxAD_CIC", "MAR_CIC_g.cm.2.yr", "MAR_CIC_err_g.cm.2.yr")
     }
-    write.table(x = output_agemodel_CIC[order(output_agemodel_CIC$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CIC.txt", sep = ""), col.names = T, row.names = F)
-    write.table(x = output_agemodel_CIC_inter[order(output_agemodel_CIC_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CIC_interpolation.txt", sep = ""), col.names = T, row.names = F)
+    write.table(x = output_agemodel_CIC[order(output_agemodel_CIC$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CIC.txt", sep = ""), col.names = T, row.names = T)
+    write.table(x = output_agemodel_CIC_inter[order(output_agemodel_CIC_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CIC_interpolation.txt", sep = ""), col.names = T, row.names = T)
 
     # Save output in the list
     out_list$'CIC age-depth model interpolated' <- output_agemodel_CIC_inter
@@ -1601,8 +1601,8 @@ serac <- function(name = "", model = c("CFCS"), Cher = NA, NWT = NA, Hemisphere 
     } else {
       colnames(output_agemodel_CRS_inter) <- c("depth_avg_mm",  "mass_depth_g.cm.2", "BestAD_CRS", "MinAD_CRS", "MaxAD_CRS", "MAR_CRS_g.cm.2.yr", "MAR_CRS_err_g.cm.2.yr")
     }
-    write.table(x = output_agemodel_CRS[order(output_agemodel_CRS$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS.txt", sep = ""), col.names = T, row.names = F)
-    write.table(x = output_agemodel_CRS_inter[order(output_agemodel_CRS_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_interpolation.txt", sep = ""), col.names = T, row.names = F)
+    write.table(x = output_agemodel_CRS[order(output_agemodel_CRS$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS.txt", sep = ""), col.names = T, row.names = T)
+    write.table(x = output_agemodel_CRS_inter[order(output_agemodel_CRS_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_interpolation.txt", sep = ""), col.names = T, row.names = T)
 
     # Save output in the list
     out_list$'CRS age-depth model interpolated' <- output_agemodel_CRS_inter
@@ -1662,8 +1662,8 @@ serac <- function(name = "", model = c("CFCS"), Cher = NA, NWT = NA, Hemisphere 
     } else {
       colnames(output_agemodel_CRS_comp_inter) <- c("depth_avg_mm", "mass_depth_g.cm.2", "BestAD_CRS_comp", "MinAD_CRS_comp", "MaxAD_CRS_comp", "MAR_CRS_comp_g.cm.2.yr", "MAR_CRS_comp_err_g.cm.2.yr")
     }
-    write.table(x = output_agemodel_CRS_comp[order(output_agemodel_CRS_comp$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_comp.txt", sep = ""), col.names = T, row.names = F)
-    write.table(x = output_agemodel_CRS_comp_inter[order(output_agemodel_CRS_comp_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_comp_interpolation.txt", sep = ""), col.names = T, row.names = F)
+    write.table(x = output_agemodel_CRS_comp[order(output_agemodel_CRS_comp$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_comp.txt", sep = ""), col.names = T, row.names = T)
+    write.table(x = output_agemodel_CRS_comp_inter[order(output_agemodel_CRS_comp_inter$depth_avg_mm, decreasing = F), ], file = paste(getwd(), "/Cores/", name, "/", name, "_CRS_comp_interpolation.txt", sep = ""), col.names = T, row.names = T)
 
     # Save output in the list
     out_list$'CRS composite model age-depth model interpolated' <- output_agemodel_CRS_comp_inter
