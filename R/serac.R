eb73bbc893f959cff049e758067f32e22193db1f
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2020-11-10 09:43:45 -0500

    change width tiff

--- R/serac.R
+++ R/serac.R
@@ -2853,7 +2853,7 @@ serac <- function(name = "", model = c("CFCS"), Cher = NA, NWT = NA, Hemisphere 
 
     # Save the plot to tiff
     if(plottiff) {
-      tiff(file = paste(getwd(), "/Cores/", name, "/", name, ".tiff", sep=""), width = (1100+2200*nwindows)*prop_width_fig, height = 4000*prop_height_fig, units = "px", res = 800)
+      tiff(file = paste(getwd(), "/Cores/", name, "/", name, ".tiff", sep=""), width = (800+1700*nwindows)*prop_width_fig, height = 4000*prop_height_fig, units = "px", res = 800)
       replayPlot(out_list$plot)
       dev.off()
     }
