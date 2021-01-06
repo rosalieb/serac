ac2d0557f2cf2c32f954722f73179804b64faf5e
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2021-01-06 10:28:25 -0500

    edit init

--- R/init.R
+++ R/init.R
@@ -3,5 +3,5 @@
   packageStartupMessage("   If you need a little help to get started, type in help_serac() or vignette('serac')")
   packageStartupMessage("   If you're using serac for the first time, you may want to use the function user_infos() to enter metadata.")
   packageStartupMessage("   We recommend you re-install the package before each use to get all recent updates, using: devtools::install_github('rosalieb/serac', build_vignettes = TRUE)")
-  packageStartupMessage("   Citation: Bruel and Sabatier (2020) Journal of Environmental Radioactivity - 10.1016/j.jenvrad.2020.106449")
+  packageStartupMessage("   Citation: Bruel and Sabatier (2020) - 10.1016/j.jenvrad.2020.106449")
 }
