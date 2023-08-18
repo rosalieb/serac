b7b4e886fb48a137d40d73a36464868d508b0138
Rosalie Bruel <rosaliebruel@gmail.com>
2023-08-18 11:24:25 +0200

    update init message

--- R/init.R
+++ R/init.R
@@ -2,6 +2,7 @@
   packageStartupMessage("This is serac, a package for ShortlivEd RAdionuclide Chronology of recent sediment cores.")
   packageStartupMessage("   If you need a little help to get started, type in help_serac() or vignette('serac')")
   packageStartupMessage("   If you're using serac for the first time, you may want to use the function user_infos() to enter metadata.")
-  packageStartupMessage("   We recommend you re-install the package before each use to get all recent updates, using: devtools::install_github('rosalieb/serac', build_vignettes = TRUE)")
+  packageStartupMessage("   We recommend you re-install the package before each use to get all recent updates, using:")
+  packageStartupMessage("      devtools::install_github('rosalieb/serac', build_vignettes = TRUE)")
   packageStartupMessage("   Citation: Bruel and Sabatier (2020) - 10.1016/j.jenvrad.2020.106449")
 }
